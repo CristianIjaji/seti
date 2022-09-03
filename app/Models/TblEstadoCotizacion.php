6dae57b726f3c32d939e382ae9d7b98ff9dd7dee
unknown <cvfigueroa@unbosque.edu.co>
2022-09-02 20:53:47 -0500

    Cambios correspondiente a la tabla de estados de cotizacion

---
+++ app/Models/TblEstadoCotizacion.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TblEstadoCotizacion extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tbl_estado_cotizacion';
+    protected $primaryKey = 'id_estado_cotizacion';
+    protected $guarded = [];
+
+    protected $filable = [
+        'id_cotizacion',
+        'estado',
+        'comentario',
+        'id_usuareg'
+    ];
+
+    public function tblCotizacion(){
+        return $this->belongsTo(tblCotizacion::class, 'id_cotizacion');
+    }
+    public function tblusuario() {
+        return $this->belongsTo(TblUsuario::class, 'id_usuareg');
+    }
+}
